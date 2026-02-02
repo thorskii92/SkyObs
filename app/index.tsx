77a22d921d9eff8dc7e564fbe29c44f17b96b82d
uzzielkyle <uzzielynciong@gmail.com>
2026-02-02 16:46:53 +0800

    Create initial files
    Add buttons and native datepicker in index.tsx file

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,22 @@
-import { Text, View } from "react-native";
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
+import { useState } from "react";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
+  const [date, setDate] = useState(new Date());
+
+  const showDatePicker = () => {
+    DateTimePickerAndroid.open({
+      value: date,
+      onChange: (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setDate(currentDate);
+      },
+      mode: 'date',
+      is24Hour: true,
+    });
+  };
+
   return (
     <View
       style={{
@@ -9,6 +25,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      Select Date: <Button title="Select Date" onPress={showDatePicker} />
+      <Button title="New" onPress={() => alert("Button pressed!")} />
+      <Button title="View" onPress={() => alert("Button pressed!")} />
+      <Button title="Generate" onPress={() => alert("Button pressed!")} />
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
